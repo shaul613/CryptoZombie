2e3daa5cbe336f43c02a46ded98ba309a89e8dcc
Shaul_Lifschitz <shaull@balink.net>
2023-07-06 04:51:41 -0400

    test

--- part_2/zombiefeeding.sol
+++ part_2/zombiefeeding.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombiefactory.sol";
 
-contract KittyInterface {
+contract KittyInterface { //test
   function getKitty(uint256 _id) external view returns (
     bool isGestating,
     bool isReady,
